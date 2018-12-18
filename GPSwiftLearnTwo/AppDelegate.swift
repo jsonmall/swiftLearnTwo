34d7b4fad00d33e4fbf56f5f845012416d1c67c3
郭鹏 <guopeng@chelun.com>
2018-12-18 22:25:10 +0800

    update

--- GPSwiftLearnTwo/AppDelegate.swift
+++ GPSwiftLearnTwo/AppDelegate.swift
@@ -24,15 +24,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //静态图引导页
         return true
     }
-
+   
+    func setStaticGuidePage()  {
+        
+        let imageNameArray : [String] = ["lead01", "lead02", "lead03"]
+        
+//        let guideView = 
+        
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
