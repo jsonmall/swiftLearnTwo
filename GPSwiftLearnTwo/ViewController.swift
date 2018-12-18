c71ee6928d567827d28169ec212e99bcd9a48182
郭鹏 <guopeng@chelun.com>
2018-12-18 21:43:34 +0800

    first commit

---
+++ GPSwiftLearnTwo/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  GpSwiftLearnOne
+//
+//  Created by 郭大侠 on 2018/12/17.
+//  Copyright © 2018年 郭大侠. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    //懒加载，私有属性
+    private lazy var tableView:UITableView = {
+        let tableView = UITableView.init(frame: self.view.frame, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellIdentifier")
+        tableView.tableFooterView = UIView.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        return tableView
+    }()
+    
+    private var dataSoure = ["漫画","网易二次元"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(self.tableView)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSoure.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
+        cell.textLabel?.text = dataSoure[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            
+            break
+        case 1:
+            break
+            
+        default: break
+            
+        }
+    }
+    
+}
+
